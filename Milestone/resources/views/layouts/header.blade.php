316509b6b8fb250facfa8ab877da947c2dc36d04
jlagunas6 <jlagunas6@my.gcu.edu>
2019-03-08 16:17:39 -0700

    fixed header display of login/username

--- Milestone/resources/views/layouts/header.blade.php
+++ Milestone/resources/views/layouts/header.blade.php
@@ -1,40 +1,57 @@
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" id="index" href="/LARAVEL_PROJECTS/Milestone/public/">Home</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse"
-			data-target="#navbarSupportedContent"
-			aria-controls="navbarSupportedContent" aria-expanded="false"
-			aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
 
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active"><a class="nav-link" href="blank">TBD <span
-						class="sr-only">(current)</span></a></li>
-				<li class="nav-item dropdown"><a class="nav-link dropdown-toggle"
-					href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-					aria-haspopup="true" aria-expanded="false"> Administration </a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" id="blank" href="blank">TBD</a> <a
-							class="dropdown-item" id="blank" href="blank">TBD</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" id="blank" href="blank">TBD</a> <a
-							class="dropdown-item" id="blank" href="blank">TBD</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<a class="navbar-brand" id="index"
+		href="/LARAVEL_PROJECTS/Milestone/public/">Home</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse"
+		data-target="#navbarSupportedContent"
+		aria-controls="navbarSupportedContent" aria-expanded="false"
+		aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
 
-					</div></li>
-				<li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a>
-				</li>
-			</ul>
-			<li class="nav-item active"><a class="nav-link" href="login">Sign In
-					<span class="sr-only">(current)</span>
-			</a></li>
-			<li class="nav-item active"><a class="nav-link" href="register">Register
-					<span class="sr-only">(current)</span>
-			</a></li>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active"><a class="nav-link" href="blank">TBD <span
+					class="sr-only">(current)</span></a></li>
+			<li class="nav-item dropdown"><a class="nav-link dropdown-toggle"
+				href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+				aria-haspopup="true" aria-expanded="false"> Administration </a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" id="blank" href="blank">TBD</a> <a
+						class="dropdown-item" id="blank" href="blank">TBD</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" id="blank" href="blank">TBD</a> <a
+						class="dropdown-item" id="blank" href="blank">TBD</a>
+
+				</div></li>
+			<li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a>
+			</li>
+		</ul>
+			
+			<?php
+
+if (session()->has('id') == NULL) {
+    echo "
+    			<li class='nav-item active'><a class='nav-link' href='login'>Sign In
+    					<span class='sr-only'>(current)</span>
+    			</a></li>
+    			<li class='nav-item active'><a class='nav-link' href='register'>Register
+    					<span class='sr-only'>(current)</span>
+    			</a></li>
+			";
+} else {
+    echo "
+                <li class='nav-item active'><a class='nav-link' href='profile'>" . session()->get('first_name') . " " . session()->get('last_name') . "
+    					<span class='sr-only'>(current)</span>
+    			</a></li>
+            ";
+}
+
+?>
 			<form class="form-inline my-2 my-lg-0" action="blank">
-				<input class="form-control mr-sm-2" type="search"
-					placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form>
-		</div>
-	</nav>
+			<input class="form-control mr-sm-2" type="search"
+				placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+	</div>
+</nav>
